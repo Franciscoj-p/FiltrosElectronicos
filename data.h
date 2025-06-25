3750ce0e2203c1daf0c17b8d30b6785909917dbe
Franciscoj-p <pfranciscoj07@gmail.com>
2025-06-25 12:32:17 -0500

    11.9.8.9

--- data.h
+++ data.h
@@ -1,6 +1,5 @@
 #include <gtk/gtk.h>
 
-bool guardarDatosIng(GtkWidget *entry_nombre, GtkWidget *entry_correo, GtkWidget *entry_id);
 bool validarEntradaNumerica(GtkWidget *entry, const char *nombreCampo);
 bool validarTexto(GtkWidget *entry, const char *nombreCampo);
 void mostrar_error(const gchar *mensaje, const gchar *titulo = "Error");
