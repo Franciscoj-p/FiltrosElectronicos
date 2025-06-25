0ec402525df0c58642a732f6c4d1d2e585b27ad1
Franciscoj-p <pfranciscoj07@gmail.com>
2025-06-25 12:32:48 -0500

    1.9.8.9

--- data.cpp
+++ data.cpp
@@ -66,30 +66,3 @@ bool validarTexto(GtkWidget *entry, const char *nombreCampo) {
     }
     return true;
 }
-
-datosIngeniero guardarDatosIng(GtkWidget *entry_nombre, GtkWidget *entry_correo, GtkWidget *entry_id) {
-    const char *nombre = gtk_entry_get_text(GTK_ENTRY(entry_nombre));
-    const char *correo = gtk_entry_get_text(GTK_ENTRY(entry_correo));
-    const char *id     = gtk_entry_get_text(GTK_ENTRY(entry_id));
-
-    if (strlen(nombre) == 0 || strlen(correo) == 0 || strlen(id) == 0) {
-        std::cerr << "Error: Todos los campos deben estar llenos." << std::endl;
-        return false;
-    }
-
-    if (strchr(correo, '@') == nullptr || strchr(correo, '.') == nullptr) {
-        std::cerr << "Error: El correo no es válido." << std::endl;
-        return false;
-    }
-
-    std::cout << "Nombre: " << nombre << std::endl;
-    std::cout << "Correo: " << correo << std::endl;
-    std::cout << "ID: " << id << std::endl;
-
-    datosIngeniero ing;
-    ing.nombre = nombre;
-    ing.correo = correo;
-    ing.id     = id;
-
-    return ing;
-}
